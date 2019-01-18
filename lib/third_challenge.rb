fa16a85a3e8610df72fbe13357afdac9c49f14d4
Sruthi Krishna <sruthik926@gmail.com>
2019-01-18 14:34:12 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -33,11 +33,11 @@ def third_challenge
       :heroine => {
         :name => "Juliet",
         :age =>"15",
+        :status => "alive",
         #your key/value pairs here
         },
       :heroine_friends => []
    }
   }
 
-  
 end
