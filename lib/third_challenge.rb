803c55ead209f7808130fcce63d2be39fa751dff
Sruthi Krishna <sruthik926@gmail.com>
2019-01-18 14:32:37 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -22,6 +22,7 @@ def third_challenge
    :capulet => {
       :patriarch => {
         :name => "Lord Capulet", 
+        :age => "51",
         #your key/value pairs here
         },
       :matriarch => {
