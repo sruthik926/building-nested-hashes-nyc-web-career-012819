d15eb4195c63027802a3bc46f2024aaddbb4cb10
Sruthi Krishna <sruthik926@gmail.com>
2019-01-18 14:29:48 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -8,7 +8,7 @@ def third_challenge
         },
       :matriarch => {
         :name => "Lady Montague",
-        :age =>
+        :age => "54",
         #your key/value pairs here
         },
       :hero => {
