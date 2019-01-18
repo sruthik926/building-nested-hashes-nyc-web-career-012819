171d105fca18bb128726247f3d1c19a646a55367
Sruthi Krishna <sruthik926@gmail.com>
2019-01-18 15:09:46 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -12,7 +12,7 @@ def fourth_challenge
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [ {:name => "Steven", :age => "30", attitude => "confused"}, {:name =>},
+      :heroine_friends => [ {:name => "Steven", :age => "30", attitude => "confused"}, {:name => "Nurse",},
         #your hashes here!
       ]
    }
